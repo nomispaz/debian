35c716c069af89b964562b04ec05a80ff2987cd4
nomispaz <nomispaz@example.org>
2025-08-18 15:37:56 +0200

    added custom kernel

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # add personal repository from opensuse buildservice
-echo 'deb http://download.opensuse.org/repositories/home:/nomispaz:/debian/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/home:nomispaz:debian.list
-curl -fsSL https://download.opensuse.org/repositories/home:nomispaz:debian/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_nomispaz_debian.gpg > /dev/null
+echo 'deb http://download.opensuse.org/repositories/home:/nomispaz:/debian/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/home:nomispaz:debian.list
+curl -fsSL https://download.opensuse.org/repositories/home:nomispaz:debian/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_nomispaz_debian.gpg > /dev/null
+
+echo 'deb http://download.opensuse.org/repositories/home:/nomispaz:/debian:/kernel/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/home:nomispaz:debian:kernel.list
+curl -fsSL https://download.opensuse.org/repositories/home:nomispaz:debian:kernel/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_nomispaz_debian_kernel.gpg > /dev/null
 
 # add brave repository
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -29,7 +32,7 @@ sudo /usr/sbin/usermod -aG libvirt simonheise
 sudo apt install podman
 
 # editors
-sudo apt install neovim emacs elpa-consult elpa-company elpa-go-mode elpa-rust-mode elpa-evil elpa-yasnippet elpa-yasnippet-snippets
+sudo apt install neovim emacs elpa-consult elpa-company elpa-go-mode elpa-markdown-mode elpa-rust-mode elpa-evil elpa-yasnippet elpa-yasnippet-snippets
 
 # fonts
 sudo apt install fonts-font-awesome fonts-dejavu
@@ -45,7 +48,7 @@ sudo apt install sway swaybg python3-dbus-next python3-i3ipc
 
 # install from personal repo
 #sudo apt install python3-nwg-panel network-manager-applet emacs-catppuccin
-sudo apt install python3-nwg-panel emacs-catppuccin cpupower-go veracrypt
+sudo apt install python3-nwg-panel emacs-catppuccin emacs-cape emacs-elixir-mode cpupower-go veracrypt
 
 # dependencies for nwg-panel
 apt install git curl bluez-tools gir1.2-gtklayershell-0.1 libgtk-3-0 pulseaudio-utils gir1.2-dbusmenu-gtk3-0.4 gir1.2-playerctl-2.0 playerctl python3-dasbus python3-gi-cairo python3-i3ipc python3-netifaces python3-psutil python3-requests python3-setuptools python3-wheel sway-notification-center
