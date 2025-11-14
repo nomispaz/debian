d8085c997f7c7038d90df90f3ce2a98c58b4574c
nomispaz <nomispaz@example.org>
2025-11-14 14:47:53 +0100

    added ostree

--- ostree/install.sh
+++ ostree/install.sh
@@ -30,6 +30,11 @@ ostree --repo=/mnt/ostree/repo commit \
     --subject="Debian sid base" \
     $BUILD
 
+mkdir -p /mnt/ostree/repo
+mkdir -p /mnt/boot
+mkdir -p /mnt/etc
+mkdir -p /mnt/var
+
 ostree admin --sysroot=/mnt deploy --os=debian debian/sid
 
 DEPLOY=/mnt/ostree/deploy/debian/deploy/$(ostree admin --sysroot=/mnt status | awk '/sid/ {print $2}')
