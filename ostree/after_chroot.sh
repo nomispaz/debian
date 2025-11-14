422c8469c1a77241a08536b58077e43666f26c09
nomispaz <nomispaz@example.org>
2025-11-14 15:26:44 +0100

    added ostree

--- ostree/after_chroot.sh
+++ ostree/after_chroot.sh
@@ -1,3 +1,16 @@
+ostree --repo=/mnt/ostree/repo init --mode=bare
+
+mv $BUILD/etc $BUILD/usr/etc
+# OSTree does not accept device nodes, sockets, or special files
+rm -rf "$BUILD"/{dev,proc,sys,run,tmp,var/tmp,mnt,media,swapfile,lost+found}
+
+ostree --repo=/mnt/ostree/repo commit \
+    --branch=debian/sid \
+    --subject="Debian sid base" \
+    $BUILD
+
+ostree admin --sysroot=/mnt os-init debian
+
 ostree admin --sysroot=/mnt deploy --os=debian debian/sid
 
 DEPLOY=/mnt/ostree/deploy/debian/deploy/$(ostree admin --sysroot=/mnt status | awk '/sid/ {print $2}')
--- ostree/install.sh
+++ ostree/install.sh
@@ -13,17 +13,12 @@ mount /dev/vda1 /mnt/boot/efi
 sudo apt install -y debootstrap ostree dracut grub-efi-amd64
 
 mkdir -p /mnt/ostree/repo
-ostree --repo=/mnt/ostree/repo init --mode=bare
 
 BUILD=/tmp/debian-buildroot
 mkdir -p $BUILD
 debootstrap --variant=minbase sid $BUILD http://deb.debian.org/debian
 
 mkdir -p $BUILD/usr
-mv $BUILD/etc $BUILD/usr/etc
-
-# OSTree does not accept device nodes, sockets, or special files
-rm -rf "$BUILD"/{dev,proc,sys,run,tmp,var/tmp,mnt,media,swapfile,lost+found}
 
 mkdir -p /mnt/ostree/repo
 mkdir -p /mnt/ostree/deploy
@@ -31,22 +26,15 @@ mkdir -p /mnt/boot
 mkdir -p /mnt/etc
 mkdir -p /mnt/var
 
-ostree --repo=/mnt/ostree/repo commit \
-    --branch=debian/sid \
-    --subject="Debian sid base" \
-    $BUILD
-
-ostree admin --sysroot=/mnt os-init debian
-
 mkdir -p $DEPLOY/{dev,proc,sys,boot/efi}
 mount --bind /dev $DEPLOY/dev
 mount --bind /proc $DEPLOY/proc
 mount --bind /sys $DEPLOY/sys
 mount --bind /mnt/boot/efi $DEPLOY/boot/efi
 
-cat > $BUILD/etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian sid main contrib non-free-firmware
-EOF
+#cat > $BUILD/etc/apt/sources.list <<EOF
+#deb http://deb.debian.org/debian sid main contrib non-free-firmware
+#EOF
 
 cp /etc/resolv.conf $BUILD/etc/
 
